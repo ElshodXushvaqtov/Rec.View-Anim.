7e6f0a688c63f70934ebc2f5bb6da66dbe394095
PC673 <elshodxushvaqtov07@gmail.com>
2023-03-29 09:21:03 +0500

    dragFlags and swipeFlags

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,23 +1,58 @@
 package com.example.myapplication
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.ItemTouchHelper.*
+import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.adapter.Player_Adapter
 import com.example.myapplication.databinding.ActivityMainBinding
+import com.example.myapplication.databinding.ItemLayoutBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        var binding=ActivityMainBinding.inflate(layoutInflater)
+        var binding = ActivityMainBinding.inflate(layoutInflater)
+        var binding2=ItemLayoutBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        var list= mutableListOf<Player>()
-        for (i in 0..10){
-        list.add(Player("Messi","Attacking Midfielder",R.drawable.messi))
-        list.add(Player("Pique","Center Back",R.drawable.pique))
-        list.add(Player("Mbappe","Right Winger",R.drawable.mbappe))
-        list.add(Player("Ronaldo","Center Forward",R.drawable.ronaldo))
-    }
-        binding.rv.adapter=Player_Adapter(list,this)
+        var list = mutableListOf<Player>()
+        for (i in 0..10) {
+            list.add(Player("Messi", "Attacking Midfielder", R.drawable.messi))
+            list.add(Player("Pique", "Center Back", R.drawable.pique))
+            list.add(Player("Mbappe", "Right Winger", R.drawable.mbappe))
+            list.add(Player("Ronaldo", "Center Forward", R.drawable.ronaldo))
+        }
 
+        val adapter = Player_Adapter(list, this)
+        val itemTouchHelper = object : ItemTouchHelper.Callback() {
+            override fun getMovementFlags(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder
+            ): Int {
+                val dragFlags = UP or DOWN or START or END
+                val swipeFlags = START or END
+                return makeMovementFlags(dragFlags, swipeFlags)
+            }
+
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                binding2.viewBg.setBackgroundColor(Color.parseColor("#121212"))
+                adapter.onItemMove(viewHolder.adapterPosition, target.adapterPosition)
+                return true
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+
+                adapter.onItemDelete(viewHolder.adapterPosition)
+            }
+        }
+        val itemTouch = ItemTouchHelper(itemTouchHelper)
+        itemTouch.attachToRecyclerView(binding.rv)
+        binding.rv.adapter = adapter
     }
 }
--- app/src/main/java/com/example/myapplication/adapter/Player_Adapter.kt
+++ app/src/main/java/com/example/myapplication/adapter/Player_Adapter.kt
@@ -12,19 +12,21 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.Player
 import com.example.myapplication.R
 import com.example.myapplication.databinding.ItemLayoutBinding
+import com.example.myapplication.util.ItemTouchAdapter
+import java.util.Collections
+class Player_Adapter(var players: MutableList<Player>, var context: Context) :
+    RecyclerView.Adapter<Player_Adapter.MyHolder>(), ItemTouchAdapter {
 
-class Player_Adapter(var players: MutableList<Player>, var context: Context):RecyclerView.Adapter<Player_Adapter.MyHolder>() {
-
-    class MyHolder(itemView: View):RecyclerView.ViewHolder(itemView){
-        var item_img=itemView.findViewById<ImageView>(R.id.img)
-        var name=itemView.findViewById<TextView>(R.id.item_name)
-        var position=itemView.findViewById<TextView>(R.id.position)
-        val main=itemView.findViewById<ConstraintLayout>(R.id.player_layout)
+    class MyHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var item_img = itemView.findViewById<ImageView>(R.id.img)
+        var name = itemView.findViewById<TextView>(R.id.item_name)
+        var position = itemView.findViewById<TextView>(R.id.position)
+        val main = itemView.findViewById<ConstraintLayout>(R.id.player_layout)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyHolder {
         return MyHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.item_layout,parent,false)
+            LayoutInflater.from(parent.context).inflate(R.layout.item_layout, parent, false)
         )
     }
 
@@ -33,13 +35,33 @@ class Player_Adapter(var players: MutableList<Player>, var context: Context):Rec
     }
 
     override fun onBindViewHolder(holder: MyHolder, position: Int) {
-        var index=players[position]
-       holder.item_img.setImageResource(index.img)
-        holder.name.text=index.name
-        holder.position.text=index.position
+        var index = players[position]
+        holder.item_img.setImageResource(index.img)
+        holder.name.text = index.name
+        holder.position.text = index.position
 
-        val anim = AnimationUtils.loadAnimation(context,R.anim.player_anim)
+        val anim = AnimationUtils.loadAnimation(context, R.anim.player_anim)
         holder.main.startAnimation(anim)
+    }
+
+    override fun onItemMove(fromPosition: Int, toPosition: Int) {
+        if (fromPosition < toPosition) {
+            for (i in fromPosition until toPosition) {
+                Collections.swap(players, i, i + 1)
+            }
+
+        }
+        else{
+            for (i in fromPosition downTo toPosition ){
+                Collections.swap(players,i,i-1)
+            }
+        }
+        notifyItemMoved(fromPosition, toPosition)
+    }
+
+    override fun onItemDelete(position: Int) {
+        players.removeAt(position)
+        notifyItemRemoved(position)
     }
 
 }
